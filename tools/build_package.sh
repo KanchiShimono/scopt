7309e25e99c2616fb09bc7fe255843f1add391f4
Kanchi Shimono <dev.kanchi.shimono@gmail.com>
2021-07-12 07:46:55 +0900

    Add package build tool

---
+++ tools/build_package.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+REPO_ROOT="$(cd "$(dirname "$0")/.."; pwd)"
+BUILD_DIR="${REPO_ROOT}/build"
+DIST_DIR="${REPO_ROOT}/dist"
+
+# enable recursive match by **
+shopt -s globstar
+cd "${REPO_ROOT}"
+
+# check directories are already exist
+if [ -e ${BUILD_DIR} ]; then
+    echo "Build directory already exists: ${BUILD_DIR}"
+    exit 1
+elif [ -e ${DIST_DIR} ]; then
+    echo "Dist directory already exists: ${DIST_DIR}"
+    exit 1
+fi
+
+# build package
+python setup.py sdist bdist_wheel
